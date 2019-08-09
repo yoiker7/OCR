ee9fb463f030b2b293c44297ecc3e9e9c896f8e2
yoiker7 <35791929+yoiker7@users.noreply.github.com>
2019-08-09 22:55:48 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -30,42 +30,7 @@ if __name__ == '__main__':
         shutil.rmtree(temp_folder)
     os.makedirs(temp_folder)
 
-    # ### 第一步：使用CV_ROI模块中的extract_ROI.py来提取出图片中的ROI，并保存到一个临时文件夹中
-    # print('start to extract_CV_roi imgs by CV_ROI module')
-    # from CV_ROI import get_save_roi
-    # CV_roi_imgs=os.path.join(temp_folder,'cv_roi_imgs')
-    # os.makedirs(CV_roi_imgs)
-    # get_save_roi(src_imgs_folder,CV_roi_imgs)   # CV_ROI get the roi
-    # print('extraxt CV_roi imgs DONE!')
-    #
-    # ### 第二步：使用CTPN_ROI模块中的extract_ROI.py来提取出图片中的ROI，并保存到另一个临时文件夹中
-    # # 下面的代码必须在Linux中运行，Windows下会出错
-    # print('start to extract CTPN_roi imgs by CTPN_ROI module')
-    # from CTPN_ROI import get_save_ctpn_roi
-    # CTPN_roi_imgs=os.path.join(temp_folder,'ctpn_roi_imgs')
-    # os.makedirs(CTPN_roi_imgs)
-    # get_save_ctpn_roi(src_imgs_folder,CTPN_roi_imgs)    # CTPN_ROI get the roi
-    # print('extract CTPN_roi imgs DONE!')
-    #
-    # ### 第三步：将提取的ROI图片整理为npy格式的数据集，并保存该数据集到临时文件夹中，用于后续CTC_Models的编码预测
-    # print('start to load CV_rois to setX.npy')
-    # from CTC_Models import load_roi_setX
-    # roiset_folder=os.path.join(temp_folder,'roiset')
-    # os.makedirs(roiset_folder)
-    #
-    # # 对CV_ROI模块得到的roi_imgs进行提取并保存为npy格式的setX
-    # cv_setX_path=os.path.join(roiset_folder,'cv_setX.npy')
-    # cv_names_path=os.path.join(roiset_folder,'cv_names.npy')
-    # load_roi_setX(CV_roi_imgs,cv_setX_path,cv_names_path,size=(80,240))
-    # print('load CV_rois DONE!')
-    #
-    # # 对CTPN_ROI模块得到的roi_imgs提取并保存为npy格式的setX
-    # ctpn_setX_path=os.path.join(roiset_folder,'ctpn_setX.npy')
-    # ctpn_names_path=os.path.join(roiset_folder,'ctpn_names.npy')
-    # load_roi_setX(CTPN_roi_imgs, ctpn_setX_path, ctpn_names_path, size=(60, 240))
-    # print('load CTPN_rois DONE!')
-
-    ### 第四步：使用Attn_sentisive_模型来预测保存到临时文件夹中
+    ### 使用Attn_sentisive_模型来预测保存到临时文件夹中
     # from demoToTxt_sen import demoToTxt0
     print('start to predict IDs of TestPic with CV_roi setX.npy,this will take some time...')
     txt_result_folder = os.path.join(temp_folder, 'txt_result')
@@ -86,7 +51,7 @@ if __name__ == '__main__':
     saved_model = 'we_need_you/150_coll_data/Attn_NoSensitive/best_accuracy.pth'
     demoToTxt2(src_imgs_folder, saved_model, Attn_txt_path)
 
-    ### 第五步：对得到的三个csv结果进行集成，得到最终结果
+    ### 对得到的三个csv结果进行集成，得到最终结果
     # from ensemble import ensemble
     ensemble(AttnSe_txt_path,CTC_txt_path,Attn_txt_path,final_txt_path)
 
